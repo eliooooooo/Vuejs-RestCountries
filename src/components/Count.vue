e5374b985ef9ba24598bc6cbce9527ba1d8a1399
Eliott <eliott.burkle@gmail.com>
2024-08-27 14:40:09 +0200

    Feat: test entier sur l'url

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -3,11 +3,13 @@ import { useRoute } from 'vue-router';
 
 const route = useRoute();
 // Récupération de la variable d'url
-const number = route.params.number
+const number = Number(route.params.number)
+const int = Number.isInteger(number)
 </script>
 
 <template>
     <div>
-        <h1>La variable d'url est {{ number }}</h1>
+        <h1 v-if="int">La variable d'url est {{ number }}</h1>
+        <h1 v-else>La variable n'est pas un entier</h1>
     </div>
 </template>
