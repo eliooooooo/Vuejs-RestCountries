cc31b97d5705c8c35a96559316b846dcad87b258
Eliott <eliott.burkle@gmail.com>
2024-08-27 11:43:33 +0200

    Feat: end boucles et tests

--- src/components/Boucles-test.vue
+++ src/components/Boucles-test.vue
@@ -1,23 +1,36 @@
-<script setup>
-const items = [
-  { id: 1, name: 'Item 1' },
-  { id: 2, name: 'Item 2' },
-  { id: 3, name: 'Item 3' },
-  { id: 4, name: 'Item 4' },
-  { id: 5, name: 'Item 5' },
-]
-</script>
-
 <template>
     <div>
         <h1>Boucles</h1>
         <h2>Exemple 1</h2>
         <ul>
-        <li v-for="(item, index) in items" :key="index">{{ item.name }}</li>
+        <li v-if="show" v-for="(item, index) in items" :key="index">{{ item.name }}</li>
+        <li v-else>Changer le mode d'affichage</li>
         </ul>
-        <h2>Exemple 2</h2>
-        <ul>
-        <li v-for="(item, index) in items" :key="index">{{ index }} - {{ item.name }}</li>
-        </ul>
+
+        <button @click="show = !show">Changer l'affichage</button>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            show: true,
+            items: [
+                { id: 1, name: 'Item 1' },
+                { id: 2, name: 'Item 2' },
+                { id: 3, name: 'Item 3' },
+                { id: 4, name: 'Item 4' },
+                { id: 5, name: 'Item 5' },
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+  button {
+    margin-top: 20px;
+    cursor: pointer;
+  }
+</style>
