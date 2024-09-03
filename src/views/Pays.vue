853ac31b40d57f75213c779a7c500dee98f4a379
Eliott <eliott.burkle@gmail.com>
2024-09-03 13:56:13 +0200

    Fix: from single to all country

--- src/views/Pays.vue
+++ src/views/Pays.vue
@@ -20,6 +20,7 @@ const fetchData = async () => {
                 return data
         })
     } else {
+        all = true;
         data.value = await fetch('https://restcountries.com/v3.1/all')
             .then(response => response.json())
             .then(data => {
